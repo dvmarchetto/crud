9f3ff2a32065d8930727ff607ae7a480095ee8e4
Davi Marchetto <davi.pazello.soares@escola.pr.gov.br>
2026-03-30 08:53:50 -0300

    Atualização 30/03/206

--- index.php
+++ index.php
@@ -14,5 +14,16 @@ $resultado = mysqli_query($conexao, $sql);
     </head>
     <body>
         <h1>Cadastro</h1>
+        <form action="salvar.php" method="post">
+            <label>Nome:</label>
+            <input type="text" name="nome" required>
+            <label>Preço:</label>
+            <input type="number" name="preco" required>
+            <label>Categoria:</label>
+            <input type="text" name="categoria" required>
+            <label>Quantidade:</label>
+            <input type="number" name="quantidade" required>
+            <button type="submit">Salvar</button>
+        </form>
     </body>
 </html>
